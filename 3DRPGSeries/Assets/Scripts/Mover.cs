8bd6396322b46db91832d640849b1739825fe663
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2023-06-14 15:33:38 -0600

    Implemented Click to move
    Uses ScreenPointToRay and  Physics.Raycast to draw a ray and then pull out the information where the ray hit, allowing the player to move to that point.

--- 3DRPGSeries/Assets/Scripts/Mover.cs
+++ 3DRPGSeries/Assets/Scripts/Mover.cs
@@ -5,20 +5,32 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] Transform target;
-    //set the object that we are using as the navmesh target destination
-    Ray lastRay;
-    //the debug ray for tracking where the player has clicked from 2D to 3D space
+    [SerializeField] NavMeshAgent PlayerNavMesh;
+        //set the component that we are using as the player navmesh
 
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //sets the last ray to the ray created from the mouse position
+            MoveToCursor();
         }
-        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
-        //draw in the editor where the ray is being shot. The 100 multiplies the length of the ray to make it more visible.
-        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //sets the ray to the ray created from the mouse position
+        RaycastHit hit;
+            //variable for where the Raycast has hit
+        bool hasHit = Physics.Raycast(ray, out hit);
+            //passing in ray and hit, retrieveing out hit and storing information on where the raycast has hit into the hit var.
+            //RaycastHit passes out a bool.
+        Debug.DrawRay(ray.origin, ray.direction * 100);
+            //draw in the editor where the ray is being shot. The 100 multiplies the length of the ray to make it more visible.
+        if (hasHit)
+        {
+            PlayerNavMesh.destination = hit.point;
+                //sets the players navmesh agent destination to the point where the ray hits an object
+        }
     }
 }
