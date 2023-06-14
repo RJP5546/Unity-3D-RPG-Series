194f793eefd2c153d6ea260a9b9527289405cb27
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2023-06-14 16:37:29 -0600

    Matched animation to movement
    Animations are matched to player velocity. Added a serialized field on the player to hash the animator and animation values for faster reference

--- 3DRPGSeries/Assets/Scripts/Mover.cs
+++ 3DRPGSeries/Assets/Scripts/Mover.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,12 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] NavMeshAgent PlayerNavMesh;
         //set the component that we are using as the player navmesh
+    [SerializeField] Animator PlayerAnimator;
+        //set the component that we are using as the player animator
+    private readonly int ForwardSpeedHash = Animator.StringToHash("ForwardSpeed");
+        //store ForwardSpeed animator value as a hash for faster refrence
+
+    
 
     void Update()
     {
@@ -14,6 +21,7 @@ public class Mover : MonoBehaviour
         {
             MoveToCursor();
         }
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -32,5 +40,19 @@ public class Mover : MonoBehaviour
             PlayerNavMesh.destination = hit.point;
                 //sets the players navmesh agent destination to the point where the ray hits an object
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = PlayerNavMesh.velocity;
+            //Gets the player nev mesh agent's global velocity
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            //takes grom global velocity to local velocity. This is because the aniomator only cares about the local
+            //velocity, regardless of position in the world
+        float speed = localVelocity.z;
+            //Sets the float speed value to the local forward(z) velocity
+        PlayerAnimator.SetFloat(ForwardSpeedHash, speed);
+            //Sets the animators speed value to the value of float speed
+
     }
 }
