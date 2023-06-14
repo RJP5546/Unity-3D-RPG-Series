f534a95beeb51bc540e1bd4db597788cf7498219
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2023-06-14 15:11:39 -0600

    Introduction to ray casting
    Added a ray that is cast from the camera to the location clicked on the screen. Used Debug commands to draw a line in the editor to see the ray being drawn

--- 3DRPGSeries/Assets/Scripts/Mover.cs
+++ 3DRPGSeries/Assets/Scripts/Mover.cs
@@ -6,9 +6,19 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
+    //set the object that we are using as the navmesh target destination
+    Ray lastRay;
+    //the debug ray for tracking where the player has clicked from 2D to 3D space
 
     void Update()
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //sets the last ray to the ray created from the mouse position
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+        //draw in the editor where the ray is being shot. The 100 multiplies the length of the ray to make it more visible.
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
