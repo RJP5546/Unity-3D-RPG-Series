fc7eb641af473e88f5be75fd529f95198730d9f1
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2023-06-15 14:27:39 -0600

    Separating the control layer
    Moved the input reader and move to cursor functions to a player controller script, that then talks to the mover script

--- 3DRPGSeries/Assets/Scripts/Mover.cs
+++ 3DRPGSeries/Assets/Scripts/Mover.cs
@@ -17,30 +17,15 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetMouseButton(0))
-        //GetMouseButton is true while the button is held,GetMouseButtonDown is true when pressed, must be pressed again to trigger again
-        {
-            MoveToCursor();
-        }
         UpdateAnimator();
     }
 
-    private void MoveToCursor()
+
+
+    public void MoveTo(Vector3 destination)
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //sets the ray to the ray created from the mouse position
-        RaycastHit hit;
-            //variable for where the Raycast has hit
-        bool hasHit = Physics.Raycast(ray, out hit);
-            //passing in ray and hit, retrieveing out hit and storing information on where the raycast has hit into the hit var.
-            //RaycastHit passes out a bool.
-        Debug.DrawRay(ray.origin, ray.direction * 100);
-            //draw in the editor where the ray is being shot. The 100 multiplies the length of the ray to make it more visible.
-        if (hasHit)
-        {
-            PlayerNavMesh.destination = hit.point;
-                //sets the players navmesh agent destination to the point where the ray hits an object
-        }
+        PlayerNavMesh.destination = destination;
+        //sets the players navmesh agent destination to the point where the ray hits an object
     }
 
     private void UpdateAnimator()
