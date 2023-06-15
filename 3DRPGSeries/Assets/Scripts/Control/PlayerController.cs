9101b19146c73131ac503c1a93515f2ff1a4e961
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2023-06-15 15:24:36 -0600

    RayCasting for components
    Added a function to make an array of all items hit by the click ray, then determine if the object clicked is a combat target or not

--- 3DRPGSeries/Assets/Scripts/Control/PlayerController.cs
+++ 3DRPGSeries/Assets/Scripts/Control/PlayerController.cs
@@ -1,3 +1,6 @@
+using RPG.Combat;
+using RPG.Movement;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,30 +17,57 @@ namespace RPG.Control
 
         private void Update()
         {
+            InteractWithCombat();
+            InteractWithMovement();
+
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            //puts everything the ray hits into an array
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                    //every object has a transform component, and a component can see any other sibling component on an object
+                if(target == null) continue;
+                //if this is true, move on to the next item in the array
+                if(Input.GetMouseButtonDown(0)) 
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void InteractWithMovement()
+        {
             if (Input.GetMouseButton(0))
             //GetMouseButton is true while the button is held,GetMouseButtonDown is true when pressed, must be pressed again to trigger again
             {
                 MoveToCursor();
             }
-
         }
 
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //sets the ray to the ray created from the mouse position
             RaycastHit hit;
             //variable for where the Raycast has hit
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             //passing in ray and hit, retrieveing out hit and storing information on where the raycast has hit into the hit var.
             //RaycastHit passes out a bool.
-            Debug.DrawRay(ray.origin, ray.direction * 100);
+            Debug.DrawRay(GetMouseRay().origin, GetMouseRay().direction * 100);
             //draw in the editor where the ray is being shot. The 100 multiplies the length of the ray to make it more visible.
             if (hasHit)
             {
                 PlayerMover.MoveTo(hit.point);
                 //passes the point where the ray hits an object as a vector3
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+            //returns the ray created from the mouse
         }
     }
 }
