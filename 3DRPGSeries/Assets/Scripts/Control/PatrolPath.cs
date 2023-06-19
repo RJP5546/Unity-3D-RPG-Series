639991f7a4b144dde0bf93ff4a01d76adb4b21fd
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2023-06-19 13:09:29 -0600

    Waypoint Loops
    Added Lines between waypoints to visualize in editor what the patrol path looks like

--- 3DRPGSeries/Assets/Scripts/Control/PatrolPath.cs
+++ 3DRPGSeries/Assets/Scripts/Control/PatrolPath.cs
@@ -10,13 +10,27 @@ namespace RPG.Control
         //sets a constant size of the waypoint gizmo for viewing in the editor
         private void OnDrawGizmos()
         {
-            for (int i = 0; i < transform.childCount; i++) 
-                //for each child component
+            for (int i = 0; i < transform.childCount; i++)
+            //for each child component
             {
-                Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmoRadius);
+                int j = GetNextIndex(i);
+                //gets the value of the next index
+                Gizmos.DrawSphere(GetWaypoint(i), waypointGizmoRadius);
                 //draw a sphere at the child component
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
             }
-            
+
+        }
+
+        private int GetNextIndex(int i)
+        {
+            return (i+1) % transform.childCount;
+            //devide i+1 by the number of children, return the remaining. (modulo) giving us the next index, preventing indexOutOfRange error
+        }
+        private Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
+            //returns the location of the child waypoint at index i
         }
     }
 }
