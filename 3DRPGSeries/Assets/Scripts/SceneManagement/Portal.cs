0493a11928c7992d02929e9346cdd54502cd194c
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2023-06-20 16:21:01 -0600

    Portal Spawn point
    Added a point that the player spawns at after going through a portal

--- 3DRPGSeries/Assets/Scripts/SceneManagement/Portal.cs
+++ 3DRPGSeries/Assets/Scripts/SceneManagement/Portal.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,8 @@ namespace RPG.SceneManagement
     {
         [SerializeField] int sceneToLoad;
         //set the int for the scene to load from the build index
+        [SerializeField] Transform spawnPoint;
+        //set the transform component of the spawn point gameobject
         
 
         private void OnTriggerEnter(Collider other)
@@ -28,8 +31,37 @@ namespace RPG.SceneManagement
             //loads next scene based off of load index value inputted, returns async operation when the scene has finished loading,
             //calling the coroutine again.
             print("Scene Loaded");
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+
             Destroy(gameObject);
             //destroys portal when its no longer needed, preventing unwanted overlap
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            //create a refrence to the player game object
+            player.transform.position = otherPortal.spawnPoint.position;
+            //set the player transform to the transform of the spawnpoint
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+            //set the player rotation to the rotation of the spawnpoint
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+                //Finds all the objects of type portal and turns them into a list.
+            {
+                if (portal == this) { continue; }
+                //if the portal is the current portal, continue to the next portal in the list
+                return portal;
+                //return the portal
+            }
+            return null;
+            //if no portals, retun null
         }
     }
 }
