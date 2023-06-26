cc9745838c0e05b2d16427751677b33fa75afdee
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2023-06-26 17:32:15 -0600

    Edited Progression
    Progression scriptable object can now take different stat fields as a separate Enum file

--- 3DRPGSeries/Assets/Scripts/Stats/Progression.cs
+++ 3DRPGSeries/Assets/Scripts/Stats/Progression.cs
@@ -8,25 +8,36 @@ public class Progression : ScriptableObject
     [SerializeField] ProgressionCharacterClass[] characterClasses = null;
     //select the character class from the editor to mark what progression tree the object will follow
 
+    public float GetHealth(CharacterClass characterClass, int level)
+    {
+        foreach (ProgressionCharacterClass progressionClass in characterClasses)
+        {
+            if (progressionClass.characterClass == characterClass)
+            {
+                //return progressionClass.health[level - 1];
+            }
+        }
+        return 0;
+    }
+
     [System.Serializable]
     //marks the ProgressionCharacterClass as serializable, and makes the classes appear in the editor
     class ProgressionCharacterClass
     {
         public CharacterClass characterClass;
         //select the character class
-        public float[] health;
+        public ProgressionStat[] stats;
+        //shows the list of stats
+        //public float[] health;
         //health array for health values as object levels up
     }
 
-    public float GetHealth(CharacterClass characterClass, int level)
+    [System.Serializable]
+    class ProgressionStat
     {
-        foreach(ProgressionCharacterClass progressionClass in characterClasses)
-        {
-            if(progressionClass.characterClass == characterClass)
-            {
-                return progressionClass.health[level-1];
-            }
-        }
-        return 0;
+        public Stat stat;
+        //tells us what stat it is
+        public float[] levels;
+        //adds the level array for the stat
     }
 }
