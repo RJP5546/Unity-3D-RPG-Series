25285d4f845a641b9a4466b833ce7e998d19613f
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2023-07-09 21:04:55 -0600

    Boom Mic and Audio Changes
    Added a boom mic to the camera, making the audio sound more true to the players location.
    
    Added a weapon Prefab for EquippedUnarmed
    
    Added sounds to the unarmed and sword attacks

--- 3DRPGSeries/Assets/Scripts/Combat/Weapon.cs
+++ 3DRPGSeries/Assets/Scripts/Combat/Weapon.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace RPG.Combat
 {
     public class Weapon : MonoBehaviour
     {
+        [SerializeField] UnityEvent onHit;
+        //unity event for when the weapon hits
         public void OnHit()
         {
-            print("Weapon Hit" + gameObject.name);
+            onHit.Invoke();
+            //invokes the onHit unity event
         }
     }
 
